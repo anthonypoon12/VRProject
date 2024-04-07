e3bb8348b4c162aaa99919d6b13bdc3f62d9260b
Wei <wlin.li2403@gmail.com>
2024-04-07 02:59:10 -0400

    set zombies to spawn randomly

---
+++ Assets/EnemySpawner.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    public GameObject theEnemy;
+    public int minxPos;
+    public int maxxPos;
+    public int minzPos;
+    public int maxzPos;
+    public int EnemyCount;
+
+    private void Start()
+    {
+        StartCoroutine(SpawnEnemies());
+    }
+
+    private IEnumerator SpawnEnemies()
+    {
+        while (EnemyCount < 20)
+        {
+            // Randomize spawn position
+            float xPos = Random.Range(minxPos, maxxPos);
+            float zPos = Random.Range(minzPos, maxzPos);
+
+            // Instantiate enemy at random position
+            Instantiate(theEnemy, new Vector3(xPos, 3, zPos), Quaternion.identity);
+
+            // Adjust spawn rate
+            float spawnRate = Random.Range(0.5f, 1f);
+            yield return new WaitForSeconds(spawnRate);
+
+            EnemyCount++;
+        }
+    }
+}
