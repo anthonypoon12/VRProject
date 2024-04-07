3ea7531936260bb38168e227467f440ed7e26b54
anthonypoon12 <anthony@poons.us>
2024-04-07 01:08:31 -0400

    adjusted sizing of camera, imported guns and bullets

---
+++ Assets/Bullet Casing/Scripts/Sound.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sound : MonoBehaviour {
+	
+	public AudioSource shell;
+
+	void Start () {
+		
+		shell = GetComponent<AudioSource>();
+		shell.Play();
+	}
+}
