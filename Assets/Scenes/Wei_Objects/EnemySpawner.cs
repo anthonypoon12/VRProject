a46aa00dee2bf5ab8b7458584c388ddc5fc8687e
anthonypoon12 <anthony@poons.us>
2024-04-09 00:34:02 -0400

    zombies now spawn on the ground

--- Assets/Scenes/Wei_Objects/EnemySpawner.cs
+++ Assets/Scenes/Wei_Objects/EnemySpawner.cs
@@ -24,10 +24,12 @@ public class EnemySpawner : MonoBehaviour
             // Randomize spawn position
             float xPos = Random.Range(minxPos, maxxPos);
             float zPos = Random.Range(minzPos, maxzPos);
+            Vector3 position = new Vector3(xPos, 0, zPos);
+            float yPos = Terrain.activeTerrain.SampleHeight(position);
 
             // Instantiate enemy at random position
             // Instantiate(theEnemy, new Vector3(minxPos, 3, minxPos), Quaternion.identity);
-            Instantiate(theEnemy, new Vector3(xPos, 100, zPos), Quaternion.identity);
+            Instantiate(theEnemy, new Vector3(xPos, yPos, zPos), Quaternion.identity);
 
             // Adjust spawn rate
             float spawnRate = Random.Range(0.5f, 1f);
